ea5ada74360ca07455d10ece09bcffd1ec916671
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-17 18:18:38 +0200

    modified:   src/features/cart/Cart.jsx 	modified:   src/features/cart/CartItem.jsx 	modified:   src/features/cart/CartOverview.jsx 	new file:   src/features/cart/DeleteItem.jsx 	modified:   src/features/cart/EmptyCart.jsx 	new file:   src/features/cart/UpdateItemQuantity.jsx 	modified:   src/features/cart/cartSlice.js 	modified:   src/features/menu/MenuItem.jsx 	modified:   src/features/user/userSlice.js 	modified:   src/ui/Button.jsx

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,15 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-  // cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      namr: "Medit",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: "Medit",
+  //     quantity: 2,
+  //     unitPrice: 16,
+  //     totalPrice: 32,
+  //   },
+  // ],
 };
 const cartSlice = createSlice({
   name: "cart",
@@ -32,8 +32,11 @@ const cartSlice = createSlice({
     decreaseItemQuantity(state, action) {
       //payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
+
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if(item.quantity === 0) cartSlice.caseReducers.deleteItem(state,action)
     },
     clearCart(state) {
       state.cart = [];
@@ -49,3 +52,14 @@ export const {
   clearCart,
 } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
