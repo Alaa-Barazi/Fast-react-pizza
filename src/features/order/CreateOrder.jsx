86162ecab7bdc0317a4fed89de4101d5f93059aa
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-17 13:51:02 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/features/cart/Cart.jsx 	new file:   src/features/cart/cartSlice.js 	modified:   src/features/menu/Menu.jsx 	modified:   src/features/order/CreateOrder.jsx 	modified:   src/features/user/CreateUser.jsx 	modified:   src/features/user/Username.jsx 	modified:   src/features/user/userSlice.js 	modified:   src/main.jsx 	new file:   src/store.js 	modified:   src/ui/Home.jsx

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector((store) => store.user.username);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
@@ -48,13 +50,13 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div className="px-4 py-6 ">
+    <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input className="input grow" type="text" name="customer" required defaultValue={username}/>
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
