91601c846e66f42bde6fb5fb807c09132f4c50a1
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-17 09:16:54 +0200

    modified:   src/features/cart/CartOverview.jsx 	modified:   src/features/user/Username.jsx 	modified:   src/ui/Header.jsx 	modified:   src/ui/Home.jsx

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Alaa</div>;
+  return <div className="hidden text-sm font-semibold md:block">Alaa</div>;
 }
 
 export default Username;
